eaef08fa2465ccaae7218fd230acaced3917957e
Maduki-tech <d.schlueter1011@gmail.com>
2026-01-03 00:28:06 +0100

    Added Example for fullscreen application

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model int
+
+type tickMsg time.Time
 
 func main() {
-	fmt.Println("Hello TUI")
+	p := tea.NewProgram(model(5), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tick()
+}
+
+// Update implements [tea.Model].
+func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := message.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	case tickMsg:
+		m--
+		if m <= 0 {
+			return m, tea.Quit
+		}
+		return m, tick()
+	}
+
+	return m, nil
+}
+
+// View implements [tea.Model].
+func (m model) View() string {
+	return fmt.Sprintf("\n\n	Hi. This Program will exit in %d seconds", m)
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
